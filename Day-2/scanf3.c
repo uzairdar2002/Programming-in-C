246cd46035b589ebac9bf495bad59a8584c785cf
Uzair Manzoor <mohduzairdarmanzoor@gmail.com>
2023-01-29 15:18:50 +0530

    Hello

---
+++ Day-2/scanf3.c
@@ -0,0 +1,31 @@
+// Addition of two numbers
+
+
+
+#include <stdio.h>
+
+void main() {
+    // int x, y, sum;
+
+    // printf("Enter Ist number: \n");
+    // scanf("%d", & x);
+    // printf("Enter Second number: \n");
+    // scanf("%d", & y);
+
+    // sum = x + y;
+
+    // printf("The sum is: %d\n", sum);
+
+
+// Or we can write it as 
+
+
+    int a, b, add;
+    printf("Enter Ist number: \n");
+    printf("Enter Second number: \n");
+
+    scanf("%d\n%d", &a, &b);
+    add = a + b;
+    printf("The sum is: %d\n", add);
+
+}
