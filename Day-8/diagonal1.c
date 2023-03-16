f84ca293853be84dceb00cd76cfe2ebcabba0ac2
Uzair Manzoor <mohduzairdarmanzoor@gmail.com>
2023-03-16 23:56:31 +0530

    Hello

---
+++ Day-8/diagonal1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Lower Diogonal
+void main()
+{
+    int arr[3][3],r , c, sum;
+    printf("ENter 2d array elements: \n");
+    for (r = 0; r < 3; r++)
+    {
+        for (c = 0; c < 3; c++)
+        {
+            scanf("%d", &arr[r][c]);
+        }
+    }
+    for (r = 0; r < 3; r++)
+    {
+        for (c = 0; c < 3; c++)
+        {
+            if (r >= c)
+            {
+                printf("%d\t", arr[r][c]);
+            } else {
+                printf("\t");
+            }
+        }
+        printf("\n");
+    }
+}    
